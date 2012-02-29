b05e7d97cf988cc92a04118b7ce3f2a4312a2fd2
Sergey Korneev <epicserg@gmail.com>
2012-02-29 22:40:46 +0200

    javascript changepicture test added.
    Signed-off-by: Sergey Korneev <epicserg@gmail.com>

--- eu/math.ut/angriestkiller/Playboard v 0.1/first_homework/JQuery/functions.js
+++ eu/math.ut/angriestkiller/Playboard v 0.1/first_homework/JQuery/functions.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	$('#front').fadeIn(1000);
 	$('#end').fadeIn(4000);
+	$('#makeBeauty').fadeIn(2000);
 	
 });
+$('#1').click(function() {
+	  $("#pic1").attr({ 'src:pictures', 'alt:pictures/missed.jpg' });
+	});
+	
---
+++ eu/math.ut/angriestkiller/Playboard v 0.1/first_homework/Javascript/MainBoard.js
@@ -0,0 +1,4 @@
+function changeSrc()
+  {
+  document.getElementById("pic2.1").src="pictures/missed.jpg";
+  }
